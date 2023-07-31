7eabcfdef4a0e0ed3d82ca61bf051c9363654b61
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-07-31 16:27:21 -0400

    corregido lo de cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3100'
+    origin: 'http://localhost:5173'
 }));
 
 const bodyParser = require('body-parser');
